8f40d6c096de860fa1ed706055d623b1c35bc453
vikash979 <vikash.kumar979@gmail.com>
2021-02-01 01:48:35 +0530

    project added

--- ciscoproject/ciscoproject/settings.py
+++ ciscoproject/ciscoproject/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'ciscoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'cisco',
-        'USER' :  'postgres',
-        'PASSWORD' : 'sudha007',
-        'HOST' : 'localhost',
-        'PORT' : 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME' : 'cisco',
+        # 'USER' :  'postgres',
+        # 'PASSWORD' : 'sudha007',
+        # 'HOST' : 'localhost',
+        # 'PORT' : 5432
     }
 }
 
---
+++ readme.txt
@@ -0,0 +1,8 @@
+create dir
+go to dir
+create a virtualenvironment
+activate virtaulenvironment
+install django, and etc all the modules listed in requirements.txt
+after that run the command python manage.py runserver
+in other terminal create superuser  by typing python manage.py createsuperuser
+
