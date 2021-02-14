d930e2d2f7d3962db06ea8389cc7e33811df58ee
vikash979 <vikash.kumar979@gmail.com>
2021-02-14 10:15:37 +0530

    project added

--- telnet.py
+++ telnet.py
@@ -1,45 +1,40 @@
-
-Question1 = """
-
-question 1
-"""
-print(Question1)
+#!/usr/bin/env python3
 import telnetlib
-import getpass
+#import getpass
 import sys
-HOST_IP = input("Enter your telnet HOST_IP: ")
-host_user = input("Enter your telnet username: ")
-password = getpass.getpass()
 
-import getpass
-import telnetlib
+HOST="localhost"
+#input("Enter your telnet HOST_IP: ")
+host_user = "vikash"
+#input("Enter your telnet username: ")
+password = "sudha"
+#getpass.getpass()
+#import getpass
 
-HOST = "http://localhost:8000/"
-user = input("Enter your remote account: ")
-password = getpass.getpass()
+#HOST = "http://localhost:8000/"
+#user = input("Enter your remote account: ")
+#password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
+tn.read_until(b"login: ")
+tn.write(host_user.encode('ascii') + b"\n")
+tn.read_until(b"Password: ")
+tn.write(password.encode('ascii') + b"\n")
+tn.write(b"touch file1\n")
+tn.write(b"zip -r /tmp/test.zip file1\n")
+tn.write(b"exit\n")
 
-tn.read_until("login: ")
-tn.write(host_user + "\n")
-if password:
-    tn.read_until("Password: ")
-    tn.write(password + "\n")
-
-tn.write("ls\n")
-tn.write("exit\n")
-
-print(tn.read_all())
+print(tn.read_all().decode('ascii'))
 
 print("\r")
 
 
-import os
-import zipfile
+#import os
+#import zipfile
 
-zf = zipfile.ZipFile("myzipfile.zip", "w")
-for dirname, subdirs, files in os.walk("mydirectory"):
-    zf.write(dirname)
-    for filename in files:
-        zf.write(os.path.join(dirname, filename))
-zf.close()
+#zf = zipfile.ZipFile("myzipfile.zip", "w")
+#for dirname, subdirs, files in os.walk("mydirectory"):
+#    zf.write(dirname)
+#    for filename in files:
+#        zf.write(os.path.join(dirname, filename))
+#zf.close()
